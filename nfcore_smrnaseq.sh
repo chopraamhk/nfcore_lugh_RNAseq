90bb0f594cbfc121b4a4ab89bcf358c340f46358
Mehak Chopra <m.chopra1@universityofgalway.ie>
2026-03-24 15:00:17 +0000

    Create nfcore_smrnaseq.sh

---
+++ nfcore_smrnaseq.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --job-name="nfcore_smRNA"
+#SBATCH -o smRNAseq.o%j
+#SBATCH -e smRNAseq.e%j
+#SBATCH --mail-user=<>
+#SBATCH --mail-type=ALL
+#SBATCH --partition="highmem"
+#SBATCH -N 1
+#SBATCH --mem=50G
+
+module load Anaconda3/2024.02-1 
+module load java/1.8.0
+conda activate nfcore
+
+nextflow run nf-core/smrnaseq --input sample_test.csv --outdir results_rnaseq --genome GRCh38 -c local.config -profile singularity --seq_center genewiz_azenta

b6880189a847dc7bcdfa03d726921af6873e7a0f
Mehak Chopra <m.chopra1@universityofgalway.ie>
2026-04-01 16:12:52 +0100

    Update RNA-seq pipeline to use smrRNA-seq

--- nfcore_rnaseq.sh
+++ nfcore_rnaseq.sh
@@ -6,11 +6,11 @@
 #SBATCH --mail-type=ALL
 #SBATCH --partition="highmem"
 #SBATCH -N 1
-
+#SBATCH --mem=50G
 
 module load Anaconda3/2024.02-1 
 module load java/1.8.0
-
 conda activate nfcore
 
-nextflow run nf-core/rnaseq --input samples.csv --outdir results_rnaseq --genome GRCh38 -profile lugh -c local.config
+nextflow run nf-core/smrnaseq --input samples.csv --outdir results_rnaseq --genome GRCh38 -c local.config -profile singularity --seq_center genewiz_azenta
+
